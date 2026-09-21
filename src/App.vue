45e3c92bb0ac6eba86d0ddb7a6c101a5c520ef90
TheTreeGuy69 <kukubaja10@gmail.com>
2026-09-21 13:46:15 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  
+  <h1>Think different Academy</h1>
+ 
+</template>
+
+<style scoped>
+
+h1 {
+  color: #000000;
+ text-align: center;
+  font-size: 100px;
+  margin-top: 150px;
+}
+  
+
+
+</style>
